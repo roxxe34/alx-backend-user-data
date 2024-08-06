bfe0df30c6905e15f75c8e58a03834afa9929d04
roxxe34 <hamzafarissi7@gmail.com>
2024-08-06 19:03:42 +0100

    `chore: Add BasicAuth class for handling basic authentication logic`

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -8,6 +8,7 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 
 
 app = Flask(__name__)
@@ -16,7 +17,7 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
 if getenv("AUTH_TYPE") == "auth":
-    auth = Auth()
+    auth = BasicAuth()
 
 
 @app.errorhandler(404)
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" BasicAuth module """
+from flask import request
+from typing import List, TypeVar
+
+
+class BasicAuth :
+    """ BasicAuth class """
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """ extract_base64_authorization_header method """
+        if authorization_header is None or type(authorization_header) is not str:
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header[6:]
