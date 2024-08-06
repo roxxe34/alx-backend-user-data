356eeaad82b6a30fa19efc1337e081c872e3c12d
Hamza Farissi <hamzafarissi7@gmail.com>
2024-08-06 13:02:40 +0100

    `chore: Add Auth class for Basic Authentication API`

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@ from typing import List, TypeVar
 
 
 class Auth:
+    """ Auth class """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Returns False """
         return False
