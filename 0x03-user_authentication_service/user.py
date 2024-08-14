420895e7b41cc2f7a7dff79301890d51cce2ab76
roxxe34 <hamzafarissi7@gmail.com>
2024-08-14 18:37:04 +0100

    `chore: Refactor User class to include __repr__ method`

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
-from sqlalchemy import Column, Integer, String
+""" User module
+"""
+
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
 
 class User(Base):
+    """
+    User class.
+    """
+
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
@@ -13,3 +20,9 @@ class User(Base):
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
+
+    def __repr__(self):
+        """
+        Str rep
+        """
+        return f"User: id={self.id}"
