f79ec38680750954a7b95412b5389202cecfcaed
roxxe34 <hamzafarissi7@gmail.com>
2024-08-14 18:24:13 +0100

    `Refactor DB class to include update_user method`

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -57,3 +57,15 @@ class DB:
             return session.query(User).filter_by(**kwargs).one()
         except Exception:
             raise NoResultFound
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """
+        update_user
+        """
+        session = self._session
+        user = self.find_user_by(id=user_id)
+        for k, v in kwargs.items():
+            if k not in VALID_FIELDS:
+                raise ValueError
+            setattr(user, k, v)
+        session.commit()
